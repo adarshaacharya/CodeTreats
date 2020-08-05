89a99610ee8baba98c4c0ee8f13e4d4c64cc815d
Aadarsha <connectwithadarsha@gmail.com>
2020-08-06 01:38:12 +0545

    :sparkles: Feat(split): Split browser into three windows

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import './App.css';
+import SplitPane, { Pane } from 'react-split-pane';
+import './styles/global.css';
 
 function App() {
-    return <div>Hello from app</div>;
+    const width = window.innerHeight;
+    return (
+        <>
+            <SplitPane split='vertical' defaultSize={width}>
+                <Pane />
+                <SplitPane split='horizontal' defaultSize={'50%'}>
+                    <Pane />
+                    <Pane />
+                </SplitPane>
+            </SplitPane>
+        </>
+    );
 }
 
 export default App;
