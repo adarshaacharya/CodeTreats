9bfc65b487cc93769e91093492b0db6565ddb6b4
Aadarsha <connectwithadarsha@gmail.com>
2020-08-07 15:53:16 +0545

    :recycle: Refactor : Map lang to extension
    function separated for mapping language to extension

--- server/controllers/code.controller.ts
+++ server/controllers/code.controller.ts
@@ -1,10 +1,12 @@
 import Axios from 'axios';
 import { Request, Response } from 'express';
+import getExtension from '../utils/lang-to-extension';
 
 export const SubmitCode = async (req: Request, res: Response) => {
     try {
-        const { sourceCode } = req.body;
-        const uri = `https://run.glot.io/languages/python/latest/`;
+        const { sourceCode, language } = req.body;
+        const extension = getExtension(language);
+        const uri = `https://run.glot.io/languages/${language}/latest/`;
 
         const axiosConfig = {
             headers: {
@@ -13,16 +15,16 @@ export const SubmitCode = async (req: Request, res: Response) => {
                 'Content-type': 'application/json',
             },
         };
-        const attributes = {
+        const data = {
             files: [
                 {
-                    name: `main.py`,
+                    name: `main.${extension}`,
                     content: sourceCode,
                 },
             ],
         };
-        const output = await Axios.post(uri, attributes, axiosConfig);
-        res.status(200).send(output.data)
+        const output = await Axios.post(uri, data, axiosConfig);
+        res.status(200).json(output.data);
     } catch (err) {
         console.log(err.message);
         res.status(500).json({ msg: 'Server error' });
---
+++ server/utils/lang-to-extension.ts
@@ -0,0 +1,14 @@
+export default function getExtension(language: string) {
+    let extension = '';
+
+    switch (language) {
+        case 'javascript':
+            extension = 'js';
+            break;
+        case 'python':
+            extension = 'py';
+            break;
+    }
+
+    return extension;
+}
