99ace0dbcb4b803f1214b41f9edc5c08ca34e4fd
Aadarsha <connectwithadarsha@gmail.com>
2020-08-13 18:37:34 +0545

    feat: add routing pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,23 @@
-import CodeEditor from 'components/CodeEditor';
-import InputPanel from 'components/InputPanel';
 import Navbar from 'components/Navbar';
-import OutputPanel from 'components/OutputPanel';
+import Home from 'pages/Home';
 import React from 'react';
-import SplitPane from 'react-split-pane';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CodeState from '_context/code/code.state';
 import './styles/global.css';
 
+
 const App: React.FC = () => {
-    const width1 = window.innerHeight / 0.8;
-    const width2 = window.innerHeight / 2;
     return (
         <>
             <CodeState>
-                <Navbar />
-                <SplitPane split='vertical' defaultSize={width1} maxSize={width1}>
-                    <CodeEditor />
-
-                    <SplitPane split='horizontal' defaultSize={width2} maxSize={width2}>
-                        <InputPanel />
-                        <OutputPanel />
-                    </SplitPane>
-                </SplitPane>
+                <Router>
+                    <>
+                    <Navbar />
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                    </Switch>
+                    </>
+                </Router>
             </CodeState>
         </>
     );
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import CodeEditor from 'components/CodeEditor';
+import InputPanel from 'components/InputPanel';
+import OutputPanel from 'components/OutputPanel';
+import React from 'react';
+import SplitPane from 'react-split-pane';
+
+
+const Home = () => {
+    const width1 = window.innerHeight / 0.8;
+    const width2 = window.innerHeight / 2;
+    return (
+        <>
+           
+                <SplitPane split='vertical' defaultSize={width1} maxSize={width1}>
+                    <CodeEditor />
+
+                    <SplitPane split='horizontal' defaultSize={width2} maxSize={width2}>
+                        <InputPanel />
+                        <OutputPanel />
+                    </SplitPane>
+                </SplitPane>
+         
+        </>
+    );
+};
+
+export default Home;
