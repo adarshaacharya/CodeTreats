6565a43798f6da075ceecd837662380c8b774438
Aadarsha <connectwithadarsha@gmail.com>
2020-08-17 03:37:18 +0545

    refactor: modal merged with form

--- client/src/components/SaveSnippet/SaveSnippet.tsx
+++ client/src/components/SaveSnippet/SaveSnippet.tsx
@@ -4,13 +4,17 @@ import { notify } from 'layout';
 import React from 'react';
 import CodeContext from '_context/code/code.context';
 import './save-snippet.style.css';
-import SaveModal from './SaveModal';
+import Modal from 'react-modal';
+import useModal from 'hooks/use-modal';
+
+Modal.setAppElement('#root');
 
 const SaveSnippet: React.FC = () => {
     const codeContext = React.useContext(CodeContext);
     const { addSnippet, code, language } = codeContext;
 
     const { playTing } = useSfx();
+    const { openModal, closeModal, modalIsOpen } = useModal(false);
 
     const initialVal = { title: '' };
     const [formData, handleInput] = useForm(initialVal);
@@ -28,23 +32,51 @@ const SaveSnippet: React.FC = () => {
 
     return (
         <>
-            <form onSubmit={onFormSubmit} autoComplete='off'>
-                <input
-                    type='text'
-                    name='title'
-                    id='title'
-                    placeholder='Type snippet title..'
-                    value={formData.title}
-                    onChange={handleInput}
-                    required
-                />
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                shouldCloseOnOverlayClick={false}
+                shouldCloseOnEsc={true}
+                closeTimeoutMS={200}
+                contentLabel='Save New Snippet'
+                className='Modal'
+                overlayClassName='Overlay'
+            >
+                <div className='header'>
+                    <p className='dark'>Enter meaningful title for snippet.</p>
+                    <button onClick={closeModal}>❌</button>
+                </div>
 
-                <button className='save__snippet' type='submit'>
-                    Save Snippet
-                </button>
-            </form>
+                <hr />
 
-            <SaveModal />
+                <div className='body'>
+                    <form onSubmit={onFormSubmit} autoComplete='off'>
+                        <input
+                            type='text'
+                            name='title'
+                            id='title'
+                            placeholder='Type snippet title..'
+                            value={formData.title}
+                            onChange={handleInput}
+                            required
+                        />
+                    </form>
+                </div>
+
+                <hr />
+                <div className='footer'>
+                    <button className='save__button' type='submit' onClick={onFormSubmit}>
+                        Save
+                    </button>
+                    <button onClick={closeModal} className='close__button'>
+                        Close
+                    </button>
+                </div>
+            </Modal>
+
+            <button className='save__snippet' onClick={openModal}>
+                Save Snippet
+            </button>
         </>
     );
 };
