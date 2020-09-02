8397a9a81a9194c2efbdfa1877a6109e472ada58
Aadarsha <connectwithadarsha@gmail.com>
2020-09-02 22:18:13 +0545

    refactor: room page protected

--- client/src/routing/routes.tsx
+++ client/src/routing/routes.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import JoinRoomPage from 'pages/JoinRoomPage';
 import RoomPage from 'pages/RoomPage';
+import RoomRoute from './RoomRoute';
 
 export const useRoutes = () => {
     return (
@@ -14,7 +15,7 @@ export const useRoutes = () => {
                 <Route exact path='/snippets' component={SnippetsPage} />
                 <Route exact path='/snippets/:id' component={SnippetPage} />
                 <Route exact path='/room' component={JoinRoomPage} />
-                <Route exact path='/room/:roomId' component={RoomPage} />
+                <RoomRoute exact path='/room/:roomId' component={RoomPage} />
             </Switch>
         </>
     );
