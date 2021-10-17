aa150c406b964c239afae7f48bfffa6e0ae2805f
Aadarsha Acharya <connectwithadarsha@gmail.com>
2021-10-17 07:59:45 +0545

    refactor: socket events imports

--- server/controllers/room.controller.ts
+++ server/controllers/room.controller.ts
@@ -1,24 +1,5 @@
 import Axios from 'axios';
-import {
-    SOCKETS_EVENTS_REALTIME_LOADING,
-    SOCKETS_EVENTS_REALTIME_MESSAGE,
-    SOCKETS_EVENTS_REALTIME_RUN,
-    SOCKETS_EVENTS_UPDATE_LOADING,
-    SOCKETS_EVENTS_UPDATE_OUTPUT,
-    SOCKETS_EVENT_CONNECTED,
-    SOCKETS_EVENT_CREATE_ROOM,
-    SOCKETS_EVENT_DISCONNECTED,
-    SOCKETS_EVENT_REALTIME_CODE,
-    SOCKETS_EVENT_REALTIME_INPUT,
-    SOCKETS_EVENT_REALTIME_LANGUAGE,
-    SOCKETS_EVENT_UPDATE_CODE,
-    SOCKETS_EVENT_UPDATE_INPUT,
-    SOCKETS_EVENT_UPDATE_LANGUAGE,
-    SOCKETS_EVENT_UPDATE_MESSAGE,
-    SOCKETS_EVENT_UPDATE_ROOM,
-    SOCKETS_EVENT_USER_JOINED,
-    SOCKETS_EVENT_USER_LEFT,
-} from '../constants/sockets';
+import * as events from '../constants/socketEvents';
 import mongoose from 'mongoose';
 import socketIO, { Socket } from 'socket.io';
 import Room from '../models/room.model';
@@ -29,15 +10,14 @@ const socketio = (server: Server) => {
     const io = socketIO(server);
 
     // socket config
-    io.on(SOCKETS_EVENT_CONNECTED, (socket: Socket) => {
+    io.on(events.SOCKETS_EVENT_CONNECTED, (socket: Socket) => {
         console.log('✅ Connected to room.');
 
         // create new room
-        socket.on(SOCKETS_EVENT_CREATE_ROOM, async (body: { roomName: string; username: string }) => {
+        socket.on(events.SOCKETS_EVENT_CREATE_ROOM, async (body: { roomName: string; username: string }) => {
             try {
                 const { roomName, username } = body;
 
-                // create db room
                 let room = new Room({
                     roomName,
                 });
@@ -53,9 +33,9 @@ const socketio = (server: Server) => {
                 const roomID = room._id.toString(); // create room id
                 socket.join(roomID); // join socket(user) in that room ID -> socket(user) is in that room
 
-                io.to(roomID).emit(SOCKETS_EVENT_UPDATE_ROOM, room); // tell io server to send this to every server within room
+                io.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_ROOM, room); // tell io server to send this to every server within room
 
-                io.to(roomID).emit(SOCKETS_EVENT_UPDATE_MESSAGE, {
+                io.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_MESSAGE, {
                     text: `${username} created chat.`,
                     sender: username,
                     notification: true,
@@ -66,7 +46,7 @@ const socketio = (server: Server) => {
         });
 
         // join a new room
-        socket.on(SOCKETS_EVENT_USER_JOINED, async (body, callback) => {
+        socket.on(events.SOCKETS_EVENT_USER_JOINED, async (body, callback) => {
             try {
                 const { roomID, username } = body;
 
@@ -84,10 +64,10 @@ const socketio = (server: Server) => {
                 room.activeUsers.push(user);
                 room = await room.save();
 
-                io.to(roomID).emit(SOCKETS_EVENT_UPDATE_ROOM, room);
+                io.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_ROOM, room);
 
-                socket.emit(SOCKETS_EVENT_UPDATE_MESSAGE, { text: `Welcome ${username}.`, notification: true });
-                socket.to(roomID).emit(SOCKETS_EVENT_UPDATE_MESSAGE, {
+                socket.emit(events.SOCKETS_EVENT_UPDATE_MESSAGE, { text: `Welcome ${username}.`, notification: true });
+                socket.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_MESSAGE, {
                     text: `${username} joined chat.`,
                     notification: true,
                     sender: username,
@@ -98,38 +78,38 @@ const socketio = (server: Server) => {
         });
 
         //code change
-        socket.on(SOCKETS_EVENT_REALTIME_CODE, body => {
+        socket.on(events.SOCKETS_EVENT_REALTIME_CODE, body => {
             try {
                 const { value, roomID } = body;
 
-                socket.broadcast.to(roomID).emit(SOCKETS_EVENT_UPDATE_CODE, value);
+                socket.broadcast.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_CODE, value);
             } catch (error) {
                 console.log(error);
             }
         });
 
         // language change
-        socket.on(SOCKETS_EVENT_REALTIME_LANGUAGE, body => {
+        socket.on(events.SOCKETS_EVENT_REALTIME_LANGUAGE, body => {
             try {
                 const { value, roomID } = body;
-                socket.broadcast.to(roomID).emit(SOCKETS_EVENT_UPDATE_LANGUAGE, value);
+                socket.broadcast.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_LANGUAGE, value);
             } catch (error) {
                 console.log(error);
             }
         });
 
         // input change
-        socket.on(SOCKETS_EVENT_REALTIME_INPUT, body => {
+        socket.on(events.SOCKETS_EVENT_REALTIME_INPUT, body => {
             try {
                 const { value, roomID } = body;
-                socket.broadcast.to(roomID).emit(SOCKETS_EVENT_UPDATE_INPUT, value);
+                socket.broadcast.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_INPUT, value);
             } catch (error) {
                 console.log(error);
             }
         });
 
         // submit code
-        socket.on(SOCKETS_EVENTS_REALTIME_RUN, async body => {
+        socket.on(events.SOCKETS_EVENTS_REALTIME_RUN, async body => {
             try {
                 const { userInput, sourceCode, language, roomID } = body;
 
@@ -154,7 +134,7 @@ const socketio = (server: Server) => {
                 };
                 const output = await Axios.post(uri, data, axiosConfig);
 
-                io.to(roomID).emit(SOCKETS_EVENTS_UPDATE_OUTPUT, output.data);
+                io.to(roomID).emit(events.SOCKETS_EVENTS_UPDATE_OUTPUT, output.data);
             } catch (error) {
                 // @ts-ignore
                 console.log(error.message);
@@ -162,7 +142,7 @@ const socketio = (server: Server) => {
         });
 
         // message
-        socket.on(SOCKETS_EVENTS_REALTIME_MESSAGE, async (body, callback) => {
+        socket.on(events.SOCKETS_EVENTS_REALTIME_MESSAGE, async (body, callback) => {
             try {
                 const { text, roomID, sender } = body;
 
@@ -178,7 +158,7 @@ const socketio = (server: Server) => {
                 room.messages.push(message);
                 await room.save();
 
-                io.to(roomID).emit(SOCKETS_EVENT_UPDATE_MESSAGE, message);
+                io.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_MESSAGE, message);
                 callback();
             } catch (error) {
                 console.log(error);
@@ -186,18 +166,18 @@ const socketio = (server: Server) => {
         });
 
         //loading
-        socket.on(SOCKETS_EVENTS_REALTIME_LOADING, (roomID: string) => {
-            io.to(roomID).emit(SOCKETS_EVENTS_UPDATE_LOADING, null);
+        socket.on(events.SOCKETS_EVENTS_REALTIME_LOADING, (roomID: string) => {
+            io.to(roomID).emit(events.SOCKETS_EVENTS_UPDATE_LOADING, null);
         });
 
         // leave room
-        socket.on(SOCKETS_EVENT_USER_LEFT, async body => {
+        socket.on(events.SOCKETS_EVENT_USER_LEFT, async body => {
             const { username, roomID } = body;
             const socketID = socket.id.toString();
 
             await Room.updateOne({}, { $pull: { activeUsers: { socketID: socketID } } });
 
-            io.to(roomID).emit(SOCKETS_EVENT_UPDATE_MESSAGE, {
+            io.to(roomID).emit(events.SOCKETS_EVENT_UPDATE_MESSAGE, {
                 text: `${username} has left room.`,
                 notification: true,
             });
@@ -205,7 +185,7 @@ const socketio = (server: Server) => {
             socket.leave(roomID);
         });
 
-        socket.on(SOCKETS_EVENT_DISCONNECTED, () => {
+        socket.on(events.SOCKETS_EVENT_DISCONNECTED, () => {
             console.log('❌ Disconnected from room.');
         });
     });
